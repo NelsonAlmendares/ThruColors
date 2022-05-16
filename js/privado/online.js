ed5ec41a04960cf48773143d21106e4b19d1c69b
KarmeYT <46569528+KarmeYT@users.noreply.github.com>
2022-05-16 00:57:31 -0600

    Programación de LogIn
    Se hace mostrar el nombre, rol e imagen del empleado que inicio sesión

--- js/privado/online.js
+++ js/privado/online.js
@@ -4,7 +4,7 @@
 */
 
 // Constante para establecer la ruta y parámetros de comunicación con la API.
-const API = SERVER + 'dashboard/empleados.php?action=';
+const API = SERVER + 'privado/empleados.php?action=';
 
 // Método manejador de eventos que se ejecuta cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', function () {
@@ -20,69 +20,20 @@ document.addEventListener('DOMContentLoaded', function () {
                 if (response.session) {
                     // Se comprueba si la respuesta es satisfactoria, de lo contrario se direcciona a la página web principal.
                     if (response.status) {
-                        const header = `
-                        <nav class="main-nav--bg">
-                            <div class="container main-nav">
-                                <div class="main-nav-start">
-                                    <div class="search-wrapper buscar">
-                                        <i data-feather="search" aria-hidden="true"></i>
-                                        <input type="text" placeholder="Buscar" required />
-                                    </div>
-                                </div>
-                                <div class="main-nav-end">
-                                    <button class="sidebar-toggle transparent-btn" title="Menu" type="button">
-                                        <span class="sr-only">Toggle menu</span>
-                                        <span class="icon menu-toggle-gray" aria-hidden="true"></span>
-                                    </button>
-                                    <button class="theme-switcher gray-circle-btn" type="button" title="Tema">
-                                        <span class="sr-only">Tema</span>
-                                        <i class="sun-icon" data-feather="sun" aria-hidden="true"></i>
-                                        <i class="moon-icon" data-feather="moon" aria-hidden="true"></i>
-                                    </button>
-                                    <div class="notification-wrapper">
-                                        <button class="gray-circle-btn dropdown-btn" title="Configuracion" type="button">
-                                            <span class="sr-only">Configuración</span>
-                                            <i class="fa-solid fa-gear"></i>
-                                        </button>
-                                        <ul class="users-item-dropdown notification-dropdown dropdown">									
-                                            <li>
-                                                <a class="sidebar-user" onclick="logOut()">Cerrar sesión</a>
-                                            </li>
-                                        </ul>
-                                    </div>							
-                                </div>
-                            </div>
-                        </nav>
-                        `;
-                        const footer = `
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col s12 m6">
-                                        <h6 class="white-text">Dashboard</h6>
-                                        <a class="white-text" href="mailto:dacasoft@outlook.com">
-                                            <i class="material-icons left">email</i>Ayuda
-                                        </a>
-                                    </div>
-                                    <div class="col s12 m6">
-                                        <h6 class="white-text">Enlaces</h6>
-                                        <a class="white-text" href="../public/" target="_blank">
-                                            <i class="material-icons left">store</i>Sitio público
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="footer-copyright">
-                                <div class="container">
-                                    <span>© 2018-2022 Copyright CoffeeShop. Todos los derechos reservados.</span>
-                                    <span class="right">Diseñado con
-                                        <a href="http://materializecss.com/" target="_blank">
-                                            <img src="../../resources/img/materialize.png" height="20" style="vertical-align:middle" alt="Materialize">
-                                        </a>
-                                    </span>
-                                </div>
-                            </div>
-                        `;
-                        //document.querySelector('header').innerHTML = header;                        
+                        const info_user = document.getElementById('info_user');
+                        const info_empleado = `
+                        <span class="sidebar-user-img">
+							<picture>
+								<source srcset="${SERVER}imagenes/empleados/${response.foto}" type="image/webp"><img
+									alt="User name">
+							</picture>
+						</span>
+						<div class="sidebar-user-info">
+							<span class="sidebar-user__title">${response.nombre}</span>
+							<span class="sidebar-user__subtitle">${response.rol}</span>
+						</div>
+                        `;                                                
+                        info_user.innerHTML = info_empleado;                        
                     } else {
                         sweetAlert(3, response.exception, 'index.html');
                     }
