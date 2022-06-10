02c4f107ae4889f9b30a55e840dde9f4cf40b444
KarmeYT <46569528+KarmeYT@users.noreply.github.com>
2022-06-09 23:26:32 -0600

    carrito terminado
    Se termina el carrito casi al 100%

--- api/models/pedidos.php
+++ api/models/pedidos.php
@@ -8,6 +8,8 @@
         private $cantidad = null;
         private $precio = null;
         private $estado = null;
+        private $marca = null;
+        private $presentacion = null;
 
     public function setIdPedido($value){
         if ($this->validateNaturalNumber($value)) {
@@ -73,18 +75,71 @@
         }
     }
 
+    public function setMarca($value){
+        if ($this->validateNaturalNumber($value)) {
+            $this->marca = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setPresentacion($value){
+        if ($this->validateNaturalNumber($value)) {
+            $this->presentacion = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public function getIdPedido(){
         return $this->id_pedido;
     }
 
+    public function getIdDetalle(){
+        return $this->id_detalle;
+    }
+
+    public function getCliente(){
+        return $this->cliente;
+    }
+
+    public function getProducto(){
+        return $this->producto;
+    }
+
+    public function getCantidad(){
+        return $this->cantidad;
+    }
+
+    public function getPrecio(){
+        return $this->precio;
+    }
+    
+    public function getEstado(){
+        return $this->estado;
+    }
+
+    public function getMarca(){
+        return $this->marca;
+    }
+
+    public function getPresentacion(){
+        return $this->presentacion;
+    }
+
     public function startOrder(){
         $this->estado = 0;
-        $sql = 'SELECT id_venta FROM PUBLIC."tb_ventas" WHERE estado_venta = ? AND id_cliente = ?';
+
+        $sql = 'SELECT id_venta FROM tb_venta WHERE estado_venta = ? AND id_cliente = ?';
         $params = array($this->estado, $_SESSION['id_cliente']);
         if($data = Database::getRow($sql, $params)){
-            $this->id_pedido = $data['id_pedido'];
+            $this->id_pedido = $data['id_venta'];
+            return true;
         } else{
-            $sql = 'INSERT INTO ventas(estado_venta, id_cliente) VALUES (?, ?)';
+            $sql = 'INSERT INTO tb_venta(fecha_venta, estado_venta, id_cliente) 
+            VALUES (CURRENT_DATE,?, ?)';
             $params = array($this->estado, $_SESSION['id_cliente']);
 
             if($this->id_pedido = Database::getLastRow($sql,$params)){
@@ -96,37 +151,49 @@
     }
 
     public function createDetail(){
-        $sql = 'INSERT INTO public."tb_detalleVenta"(
-                cantidad, id_producto, id_venta, id_valoracion)
-                VALUES (?, (SELECT costo_producto FROM tb_ventas WHERE id_venta = ?), ?, ?)';
-        $params = array($this->producto, $this->producto, $this->cantidad, $this->id_pedido);
+        $sql = 'INSERT INTO "tb_DetalleVenta"(
+                cantidad,precio_producto, id_producto, id_venta)
+                VALUES (?, (SELECT costo_producto FROM tb_producto WHERE id_producto = ?), ?,?)';
+        $params = array($this->cantidad, $this->producto, $this->producto, $this->id_pedido);
         return Database::executeRow($sql, $params);
     }
 
     public function readOrderDetail(){
-        $sql = 'SELECT id_venta, nombre_producto, costo_producto, cantidad
-        FROM public."tb_detalleVenta" td INNER JOIN tb_producto tp ON td.id_producto = tp.id_producto
-        WHERE id_venta = ?';
+        $sql = 'SELECT id_venta, nombre_producto AS nombre, costo_producto AS costo, cantidad, tm.nombre_marca AS marca, tb.presentacion_producto AS presentacion, td."id_DetalleVenta" AS id_detalle
+                FROM "tb_DetalleVenta" td INNER JOIN tb_producto tp ON td.id_producto = tp.id_producto
+                INNER JOIN tb_marca tm ON tp."id_marcaProducto" = tm.id_marca
+                INNER JOIN tb_presentacion tb ON tp."id_presentacionProducto" = tb.id_presentacion
+                WHERE id_venta = ?';
         $params = array($this->id_pedido);
         return Database::getRows($sql, $params);
     }
 
     public function finishOrder(){
-        date_default_timezone_get('America/El_Salvador');
         $date = date('Y-m-d');
         $this->estado = 1;
-        $sql = 'UPDATE public.tb_ventas
-            SET fecha_venta=?, estado_venta=?
+        $sql = 'UPDATE tb_venta
+            SET fecha_venta=CURRENT_DATE, estado_venta=?
             WHERE id_venta = ?';
-        $params = array($this->estado, $date, $_SESSION['id_venta']);
+        $params = array($this->estado, $_SESSION['id_pedido']);
         return Database::executeRow($sql, $params);
     }
 
-    public function updateDetail(){
-        $sql = 'UPDATE public."tb_detalleVenta"
-        SET cantidad=?
-        WHERE "id_detalleVenta" = ? AND id_venta = ?';
-        
+    public function updateDetail()
+    {
+        $sql = 'UPDATE "tb_DetalleVenta"
+                SET cantidad = ?
+                WHERE "id_DetalleVenta" = ? AND id_venta = ?';
+        $params = array($this->cantidad, $this->id_detalle, $this->id_pedido);
+        return Database::executeRow($sql, $params);
+    }
+
+    // Método para eliminar un producto que se encuentra en el carrito de compras.
+    public function deleteDetail()
+    {
+        $sql = 'DELETE FROM "tb_DetalleVenta"
+                WHERE "id_DetalleVenta" = ? AND id_venta = ?';
+        $params = array($this->id_detalle, $_SESSION['id_pedido']);
+        return Database::executeRow($sql, $params);
     }
 
     }
--- controller/public/detalle_p.js
+++ controller/public/detalle_p.js
@@ -1,5 +1,6 @@
 const API_CATALOGO = SERVER + 'publico/catalogo.php?action=';
 const API_VALORACIONES = SERVER  + 'publico/valoracion.php?action=';
+const API_PEDIDOS = SERVER  + 'publico/pedidos.php?action=';
 
 // Método manejador de eventos que se ejecuta cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', function () {
@@ -52,7 +53,9 @@ function readOneProducto(id) {
                         document.getElementById('img-container').innerHTML = foto;
                         document.getElementById('producto-container').innerHTML = producto;
                         document.getElementById('marca-container').innerHTML = marca;
-                        document.getElementById('presentacion-container').innerHTML = presentacion;                                       
+                        document.getElementById('presentacion-container').innerHTML = presentacion;
+                         // Se asigna el valor del id del producto al campo oculto del formulario.
+                        document.getElementById('id_producto').value = response.dataset.id;                                                                                   
                 } else {
                     // Se presenta un mensaje de error cuando no existen datos para mostrar.
                     document.getElementById('detalle-producto').innerHTML = `<i class="material-icons small">cloud_off</i><span class="red-text"> ${response.exception}</span>`;
@@ -252,3 +255,34 @@ function fillBadgeValoracion(){
     });
     document.getElementById('badge').innerHTML = content;
 }
+
+// Método manejador de eventos que se ejecuta cuando se envía el formulario de agregar un producto al carrito.
+document.getElementById('carrito-form').addEventListener('submit', function (event) {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Petición para agregar un producto al pedido.
+    fetch(API_PEDIDOS + 'createDetail', {
+        method: 'post',
+        body: new FormData(document.getElementById('carrito-form'))
+    }).then(function (request) {
+        // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje en la consola indicando el problema.
+        if (request.ok) {
+            // Se obtiene la respuesta en formato JSON.
+            request.json().then(function (response) {
+                // Se comprueba si la respuesta es satisfactoria, de lo contrario se constata si el cliente ha iniciado sesión.
+                if (response.status) {
+                    sweetAlert(1, response.message, 'carrito.html');
+                } else {
+                    // Se verifica si el cliente ha iniciado sesión para mostrar la excepción, de lo contrario se direcciona para que se autentique. 
+                    if (response.session) {
+                        sweetAlert(2, response.exception, null);
+                    } else {
+                        sweetAlert(3, response.exception, 'login.html');
+                    }
+                }
+            });
+        } else {
+            console.log(request.status + ' ' + request.statusText);
+        }
+    });
+});
